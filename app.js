1ad7bb7373697ce6065c7b63cfc2ec69db7470d1
RomelLopez <romelanthonylopez@gmail.com>
2022-06-14 17:59:50 -0700

    fade in update

---
+++ app.js
@@ -0,0 +1,19 @@
+function reveal() {
+    const reveals = document.querySelectorAll('.reveal');
+    for (let i = 0; i < reveals.length; i++) {
+        const windowHeight = window.innerHeight;
+        const elementTop = reveals[i].getBoundingClientRect().top;
+        const elementVisible = 75;
+
+        if (elementTop < windowHeight - elementVisible) {
+            reveals[i].classList.add('active');
+        } else {
+            reveals[i].classList.remove('active');
+        }
+    };
+};
+
+reveal()
+
+
+window.addEventListener('scroll', reveal);
